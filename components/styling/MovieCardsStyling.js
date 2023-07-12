76f20010f5da65c801c620dd41e54a4b24e1f802
Alex <alexander.unmuth@gmail.com>
2023-07-12 15:24:07 +0200

    fixing - crypting API_KEY

--- components/styling/MovieCardsStyling.js
+++ components/styling/MovieCardsStyling.js
@@ -1,17 +1,27 @@
 import styled from "styled-components";
 
 export const StyledMovieCard = styled.div`
-  background-color: darkred;
+  background-image: linear-gradient(
+    to bottom,
+    #5a0000,
+    #6b0804,
+    #7d1405,
+    #8f1f04,
+    #a02b00
+  );
   border-radius: 20px;
   box-shadow: 5px 10px 5px 0px rgba(0, 0, 0, 0.15);
-  padding: 20px;
-  margin-bottom: 30px;
-  width: 300px;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+  width: 250px;
 `;
 
 export const StyledMovieImage = styled.div`
   border-radius: 20px;
   display: flex;
+  width: auto;
+  height: 100%;
   justify-content: center;
   margin-bottom: 10px;
 `;
@@ -24,4 +34,11 @@ export const StyledMovieTitle = styled.div`
   margin-bottom: 10px;
   text-align: center;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+`;
+
+export const MovieCardsList = styled.ul`
+  padding-inline: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
